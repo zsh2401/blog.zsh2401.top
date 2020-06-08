ac74866647e8464996bdbf2b09797edd65727639
zsh2401 <zsh2401@163.com>
2020-06-09 02:39:18 +0800

    使用icarus v3.1版本主题

---
+++ themes/icarus-3.1.0/layout/common/footer.jsx
@@ -0,0 +1,94 @@
+const { Component } = require('inferno');
+const { cacheComponent } = require('hexo-component-inferno/lib/util/cache');
+
+class Footer extends Component {
+    render() {
+        const {
+            logo,
+            logoUrl,
+            siteUrl,
+            siteTitle,
+            siteYear,
+            author,
+            links,
+            showVisitorCounter,
+            visitorCounterTitle
+        } = this.props;
+
+        return <footer class="footer">
+            <div class="container">
+                <div class="level">
+                    <div class="level-start">
+
+                        <div style={{
+                            width:"300px",
+                            margin:"0 auto",
+                            padding:"5px 0"
+                        }}>
+                            <img src="/images/beian/gongan.png"></img>
+                            <a target="_blank"
+                                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=53018102000119"
+                                style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+                                <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">滇公网安备
+                                    53018102000119号</p>
+                            </a>
+                        </div>
+
+                        <a target="_blank" href="http://www.beian.miit.gov.cn/">滇ICP备19011321号</a><br />
+
+                        <p class="size-small">
+                            <span dangerouslySetInnerHTML={{ __html: `&copy; 2017-${siteYear} ${author || siteTitle}` }}></span>
+                            &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank" rel="noopener">Hexo</a>&nbsp;&&nbsp;
+                            <a href="https://github.com/ppoffice/hexo-theme-icarus" target="_blank" rel="noopener">Icarus</a>
+                            {showVisitorCounter ? <br /> : null}
+                            {showVisitorCounter ? <span id="busuanzi_container_site_uv"
+                                dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span> : null}
+                        </p>
+
+                    </div>
+                    <div class="level-end">
+                        {Object.keys(links).length ? <div class="field has-addons">
+                            {Object.keys(links).map(name => {
+                                const link = links[name];
+                                return <p class="control">
+                                    <a class={`button is-transparent ${link.icon ? 'is-large' : ''}`} target="_blank" rel="noopener" title={name} href={link.url}>
+                                        {link.icon ? <i class={link.icon}></i> : name}
+                                    </a>
+                                </p>;
+                            })}
+                        </div> : null}
+                    </div>
+                </div>
+            </div>
+        </footer>;
+    }
+}
+
+module.exports = cacheComponent(Footer, 'common.footer', props => {
+    const { config, helper } = props;
+    const { url_for, _p, date } = helper;
+    const { logo, title, author, footer, plugins } = config;
+
+    const links = {};
+    if (footer && footer.links) {
+        Object.keys(footer.links).forEach(name => {
+            const link = footer.links[name];
+            links[name] = {
+                url: url_for(typeof link === 'string' ? link : link.url),
+                icon: link.icon
+            };
+        });
+    }
+
+    return {
+        logo,
+        logoUrl: url_for(logo),
+        siteUrl: url_for('/'),
+        siteTitle: title,
+        siteYear: date(new Date(), 'YYYY'),
+        author,
+        links,
+        showVisitorCounter: plugins && plugins.busuanzi === true,
+        visitorCounterTitle: _p('plugin.visitor', '<span id="busuanzi_value_site_uv">0</span>')
+    };
+});
